e4c18da83a17b0518df8ca78e4d88633f7d4c59e
Magnus Brantheim <m.brantheim@gmail.com>
2021-11-29 23:22:34 +0100

    Updated names to xshare and tshare

--- contracts/XShare.sol
+++ contracts/XShare.sol
@@ -8,64 +8,64 @@ import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
 contract XShare is ERC20("XSHARE", "XSHARE") {
     using SafeMath for uint256;
-    IERC20 public boo;
+    IERC20 public tshare;
 
-    // Define the Boo token contract
-    constructor(IERC20 _boo) public {
-        boo = _boo;
+    // Define the tshare token contract
+    constructor(IERC20 _tshare) public {
+        tshare = _tshare;
     }
 
-    // Locks Boo and mints xBoo
+    // Locks tshare and mints xshare
     function enter(uint256 _amount) public {
-        // Gets the amount of Boo locked in the contract
-        uint256 totalBoo = boo.balanceOf(address(this));
-        // Gets the amount of xBoo in existence
-        uint256 totalShares = totalSupply();
-        // If no xBoo exists, mint it 1:1 to the amount put in
-        if (totalShares == 0 || totalBoo == 0) {
+        // Gets the amount of tshare locked in the contract
+        uint256 totalTShare = tshare.balanceOf(address(this));
+        // Gets the amount of xshare in existence
+        uint256 totalXShare = totalSupply();
+        // If no xshare exists, mint it 1:1 to the amount put in
+        if (totalXShare == 0 || totalTShare == 0) {
             mint(msg.sender, _amount);
         } 
-        // Calculate and mint the amount of xBoo the Boo is worth. The ratio will change overtime, as xBoo is burned/minted and Boo deposited + gained from fees / withdrawn.
+        // Calculate and mint the amount of xshare the tshare is worth. The ratio will change overtime, as xshare is burned/minted and tshare deposited + gained from fees / withdrawn.
         else {
-            uint256 what = _amount.mul(totalShares).div(totalBoo);
+            uint256 what = _amount.mul(totalXShare).div(totalTShare);
             mint(msg.sender, what);
         }
-        // Lock the Boo in the contract
-        boo.transferFrom(msg.sender, address(this), _amount);
+        // Lock the tshare in the contract
+        tshare.transferFrom(msg.sender, address(this), _amount);
     }
 
-    // Unlocks the staked + gained Boo and burns xBoo
-    function leave(uint256 _share) public {
-        // Gets the amount of xBoo in existence
-        uint256 totalShares = totalSupply();
-        // Calculates the amount of Boo the xBoo is worth
-        uint256 what = _share.mul(boo.balanceOf(address(this))).div(totalShares);
-        burn(msg.sender, _share);
-        boo.transfer(msg.sender, what);
+    // Unlocks the staked + gained tshare and burns xshare
+    function leave(uint256 _xshare) public {
+        // Gets the amount of xshare in existence
+        uint256 totalXShare = totalSupply();
+        // Calculates the amount of tshare the xshare is worth
+        uint256 what = _xshare.mul(tshare.balanceOf(address(this))).div(totalXShare);
+        burn(msg.sender, _xshare);
+        tshare.transfer(msg.sender, what);
     }
 
-    // returns the total amount of BOO an address has in the contract including fees earned
-    function BOOBalance(address _account) external view returns (uint256 booAmount_) {
-        uint256 xBOOAmount = balanceOf(_account);
-        uint256 totalxBOO = totalSupply();
-        booAmount_ = xBOOAmount.mul(boo.balanceOf(address(this))).div(totalxBOO);
+    // returns the total amount of tshare an address has in the contract including fees earned
+    function tshareBalance(address _account) external view returns (uint256 tshareAmount_) {
+        uint256 xshareAmount = balanceOf(_account);
+        uint256 totalxshare = totalSupply();
+        tshareAmount_ = xshareAmount.mul(tshare.balanceOf(address(this))).div(totalxshare);
     }
 
-    // returns how much BOO someone gets for redeeming xBOO
-    function xBOOForBOO(uint256 _xBOOAmount) external view returns (uint256 booAmount_) {
-        uint256 totalxBOO = totalSupply();
-        booAmount_ = _xBOOAmount.mul(boo.balanceOf(address(this))).div(totalxBOO);
+    // returns how much tshare someone gets for redeeming xshare
+    function xshareForTShare(uint256 _xshareAmount) external view returns (uint256 tshareAmount_) {
+        uint256 totalxshare = totalSupply();
+        tshareAmount_ = _xshareAmount.mul(tshare.balanceOf(address(this))).div(totalxshare);
     }
 
-    // returns how much xBOO someone gets for depositing BOO
-    function BOOForxBOO(uint256 _booAmount) external view returns (uint256 xBOOAmount_) {
-        uint256 totalBoo = boo.balanceOf(address(this));
-        uint256 totalxBOO = totalSupply();
-        if (totalxBOO == 0 || totalBoo == 0) {
-            xBOOAmount_ = _booAmount;
+    // returns how much xshare someone gets for depositing tshare
+    function tshareForxshare(uint256 _tshareAmount) external view returns (uint256 xshareAmount_) {
+        uint256 totaltshare = tshare.balanceOf(address(this));
+        uint256 totalxshare = totalSupply();
+        if (totalxshare == 0 || totaltshare == 0) {
+            xshareAmount_ = _tshareAmount;
         }
         else {
-            xBOOAmount_ = _booAmount.mul(totalxBOO).div(totalBoo);
+            xshareAmount_ = _tshareAmount.mul(totalxshare).div(totaltshare);
         }
     }
 
