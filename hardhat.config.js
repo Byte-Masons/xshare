7a58e1c17afe256add775f862ed4ad335f0396bc
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2021-12-02 22:28:53 +0100

    added base for masonry crypt strategy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+// eslint-disable-next-line no-undef
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -17,5 +18,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4'
+      },
+      {
+        version: '0.6.0'
+      },
+      {
+        version: '0.6.2'
+      }
+    ]
+  }
 };
