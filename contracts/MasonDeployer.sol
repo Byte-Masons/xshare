f32efa3fe619e00a018afdf4798e01d995780f80
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-01-06 20:51:31 +0100

    More comments

--- contracts/MasonDeployer.sol
+++ contracts/MasonDeployer.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.6.0;
 
 import './Mason.sol';
 
+/**
+ * @dev Implementation of a deployer that will deploy 'masons' contracts to be operated by a Strategy
+ * The masons can only be operated by the Strategy, they handle all Masonry interactions.
+ */
 contract MasonDeployer {
     address[] masons;
 
+    /**
+     * @dev Deploys multiple masons
+     */
     function deployMasons(uint256 _total) external returns (address[] memory) {
         delete masons;
         address mason;
@@ -16,6 +23,9 @@ contract MasonDeployer {
         return masons;
     }
 
+    /**
+     * @dev Deploys a single mason and assigns it the Strategy as the only authorized party 
+     */
     function deployMason() public returns (address) {
         address mason = address(new Mason(msg.sender));
         return mason;
